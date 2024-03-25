569caefbf36c935f73b7cda488e61ecaf92be1fe
Jayden Harrod <jayden@camplify.com.au>
2024-03-25 21:52:56 +1100

    add list header

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -2,6 +2,8 @@
 
 import { ListWithCards } from "@/types";
 import { ListForm } from "./list-form";
+import { useEffect, useState } from "react";
+import { ListItem } from "./list-item";
 
 interface ListContainerProps {
   data: ListWithCards[];
@@ -9,8 +11,17 @@ interface ListContainerProps {
 }
 
 export const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  const [orderedLists, setOrderedLists] = useState(data);
+
+  useEffect(() => {
+    setOrderedLists(data);
+  }, [data]);
+
   return (
-    <ol>
+    <ol className="flex gap-x-3 h-full">
+      {orderedLists.map((list, index) => (
+        <ListItem key={list.id} index={index} data={list} />
+      ))}
       <ListForm />
       <div className="flex-shrink-0 w-1" />
     </ol>
