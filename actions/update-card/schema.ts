c2e096ad59b4a624f7232c5f7360d180adf75e6a
Jayden Harrod <jayden@camplify.com.au>
2024-03-26 00:58:11 +1100

    implement card actions

--- actions/update-card/schema.ts
+++ actions/update-card/schema.ts
@@ -3,14 +3,16 @@ import { z } from "zod";
 export const UpdateCard = z.object({
   id: z.string(),
   boardId: z.string(),
-  title: z
-    .string({
-      required_error: "Title is required",
-      invalid_type_error: "Title must be a string",
-    })
-    .min(3, {
-      message: "Title must be at least 3 characters long",
-    }),
+  title: z.optional(
+    z
+      .string({
+        required_error: "Title is required",
+        invalid_type_error: "Title must be a string",
+      })
+      .min(3, {
+        message: "Title must be at least 3 characters long",
+      })
+  ),
   description: z.optional(
     z
       .string({
