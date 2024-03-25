752e21f17debd9851636bb845f5ba62456e1a70e
Jayden Harrod <jayden@camplify.com.au>
2024-03-26 00:19:42 +1100

    add card header

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { useCardModal } from "@/hooks/use-card-modal";
 import { fetcher } from "@/lib/fetcher";
 import { CardWithList } from "@/types";
-import { useQuery } from "@tanstack/react-query";
+import { Header } from "./header";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
@@ -18,7 +19,9 @@ export const CardModal = () => {
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent>{cardData?.title}</DialogContent>
+      <DialogContent>
+        {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+      </DialogContent>
     </Dialog>
   );
 };
